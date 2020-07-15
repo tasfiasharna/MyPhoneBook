705ae44758789719078eb27bbb567147c013f1d1
tasfiasharna <tasfichy786@gmail.com>
2020-07-16 00:20:01 +0600

    Initial commit

---
+++ app/src/main/java/com/tasfia/myphonebook/LogInActivity.java
@@ -0,0 +1,190 @@
+package com.tasfia.myphonebook;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LogInActivity extends AppCompatActivity {
+
+    //EditText object declaration
+    EditText etxtCell,etxtPassword;
+    //Button object declaration
+    Button btnSignUp,btnLogin;
+    //ProgressDialog object declaration
+    private ProgressDialog loading;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+
+
+
+        //Link up to java object to xml Button id
+        btnSignUp=(Button)findViewById(R.id.btn_signup);
+        btnLogin=(Button)findViewById(R.id.btn_login);
+
+
+        //Link up to java object to xml EditText id
+        etxtCell=(EditText)findViewById(R.id.etxt_cell);
+        etxtPassword=(EditText)findViewById(R.id.etxt_password);
+
+        //Click listener in Login Button
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Call login function
+                login();
+
+            }
+        });
+
+        //Click listener in Signup Button
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(LogInActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+    //Login function
+    private void login() {
+        //Getting values from edit texts
+        final String cell = etxtCell.getText().toString().trim();
+        final String password = etxtPassword.getText().toString().trim();
+
+
+        //Checking usercell field/validation
+        if (cell.isEmpty()) {
+            etxtCell.setError("Please enter cell !");
+            requestFocus(etxtCell);
+        }
+
+
+
+        //Checking password field/validation
+        else if (password.isEmpty()) {
+            etxtPassword.setError("Password can't be empty!");
+            requestFocus(etxtPassword);
+        }
+
+        //showing progress dialog
+
+        else {
+
+            loading = new ProgressDialog(this);
+            // loading.setIcon(R.drawable.wait_icon);
+            loading.setTitle("Login");
+            loading.setMessage("Please wait....");
+            loading.show();
+
+            //Creating a string request
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, Constant.LOGIN_URL,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+
+                            Log.d("Response",""+response);
+                            //If we are getting success from server
+                            if (response.equals("success")) {
+                                //Creating a shared preference
+
+                                SharedPreferences sp = LogInActivity.this.getSharedPreferences(Constant.SHARED_PREF_NAME, Context.MODE_PRIVATE);
+
+                                //Creating editor to store values to shared preferences
+                                SharedPreferences.Editor editor = sp.edit();
+                                //Adding values to editor
+                                editor.putString(Constant.CELL_SHARED_PREF, cell);
+
+                                //Saving values to editor
+                                editor.commit();
+
+                                loading.dismiss();
+                                //Starting Home activity
+                                Intent intent = new Intent(LogInActivity.this, HomeActivity.class);
+                                startActivity(intent);
+                                Toast.makeText(LogInActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+
+                            }
+
+
+
+
+                            else if(response.equals("failure")) {
+                                //If the server response is not success
+                                //Displaying an error message on toast
+                                Toast.makeText(LogInActivity.this, "Invalid Login", Toast.LENGTH_LONG).show();
+                                loading.dismiss();
+                            }
+
+                            else {
+                                //If the server response is not success
+                                //Displaying an error message on toast
+                                Toast.makeText(LogInActivity.this, "Invalid user cell or password", Toast.LENGTH_LONG).show();
+                                loading.dismiss();
+                            }
+                        }
+                    },
+
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            //You can handle error here if you want
+
+                            Toast.makeText(LogInActivity.this, "There is an error !!!", Toast.LENGTH_LONG).show();
+                            loading.dismiss();
+                        }
+                    }) {
+
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    Map<String, String> params = new HashMap<>();
+                    //Adding parameters to request
+                    params.put(Constant.KEY_CELL, cell);
+                    params.put(Constant.KEY_PASSWORD, password);
+
+                    //returning parameter
+                    return params;
+                }
+            };
+
+            //Adding the string request to the queue
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+            requestQueue.add(stringRequest);
+        }
+
+    }
+
+    //for request focus
+    private void requestFocus(View view) {
+        if (view.requestFocus()) {
+            getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+        }
+    }
+}
+
+
---
+++ app/src/main/java/com/tasfia/myphonebook/SignUpActivity.java
@@ -0,0 +1,156 @@
+package com.tasfia.myphonebook;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SignUpActivity extends AppCompatActivity {
+
+
+    //Declaring Java EditText objects
+    EditText etxtName,etxtCell,etxtPassword;
+    //Declaring Java Button objects
+    Button btnSignUp;
+    ProgressDialog loading;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        //Assigning java object to xml edit text fields
+        etxtName=(EditText)findViewById(R.id.etxt_name);
+        etxtCell=(EditText)findViewById(R.id.etxt_cell);
+        etxtPassword=(EditText)findViewById(R.id.etxt_password);
+
+        //Assigning java object to xml edit text fields
+        btnSignUp=(Button)findViewById(R.id.btn_signup);
+
+        //set click listener in Sign up button
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                sign_up();
+
+            }
+        });
+    }
+    private void sign_up()
+    {
+        //Getting values from edit texts
+        final String name = etxtName.getText().toString().trim();
+        final String cell = etxtCell.getText().toString().trim();
+        final String password = etxtPassword.getText().toString().trim();
+
+
+        //Checking  field/validation
+        if (name.isEmpty()) {
+            etxtName.setError("Please enter name !");
+           etxtName.requestFocus();
+        }
+        else if (cell.length()!=11) {
+
+            etxtCell.setError("Please enter cell !");
+            etxtCell.requestFocus();
+
+        }
+
+
+        else if (password.isEmpty()) {
+
+            etxtPassword.setError("Please enter password !");
+            etxtPassword.requestFocus();
+
+        }
+
+        else
+        {
+            loading = new ProgressDialog(this);
+            loading.setIcon(R.drawable.wait_icon);
+            loading.setTitle("Sign up");
+            loading.setMessage("Please wait....");
+            loading.show();
+
+
+            StringRequest stringRequest=new StringRequest(Request.Method.POST, Constant.SIGNUP_URL, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+
+                    //for track response in logcat
+                    Log.d("RESPONSE", response);
+
+                    if (response.equals("success")) {
+                        loading.dismiss();
+                        Intent intent = new Intent(SignUpActivity.this, LogInActivity.class);
+                        Toast.makeText(SignUpActivity.this, "Sign up successful", Toast.LENGTH_SHORT).show();
+                        startActivity(intent);
+                    } else if (response.equals("exists")) {
+
+                        Toast.makeText(SignUpActivity.this, "User already exists!", Toast.LENGTH_SHORT).show();
+                        loading.dismiss();
+
+                    }
+
+                    else if (response.equals("failure")) {
+
+                        Toast.makeText(SignUpActivity.this, "Registration Failed!", Toast.LENGTH_SHORT).show();
+                        loading.dismiss();
+
+                    }
+                }
+            },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+
+                            Toast.makeText(SignUpActivity.this, "No Internet Connection or \nThere is an error !!!", Toast.LENGTH_LONG).show();
+                            loading.dismiss();
+                        }
+                    }
+
+            ){
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    Map<String, String> params = new HashMap<>();
+                    //Adding parameters to request
+
+                    params.put(Constant.KEY_NAME, name);
+                    params.put(Constant.KEY_CELL, cell);
+                    params.put(Constant.KEY_PASSWORD, password);
+
+
+                    Log.d("info",""+name+" "+cell);
+
+                    //returning parameter
+                    return params;
+                }
+            };
+
+
+            //Adding the string request to the queue
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+            requestQueue.add(stringRequest);
+        }
+
+    }
+
+    }
